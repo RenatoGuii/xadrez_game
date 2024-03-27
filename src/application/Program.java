f4b067d3f6f5f87425834dd69c41da60000a3e1b
Renato Guimarães <renatoguidev@gmail.com>
2024-03-27 11:26:13 -0300

    Tratando exceções e limpando a tela

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import xadrez.PartidaXadrez;
 import xadrez.PecaXadrez;
 import xadrez.PosicaoXadrez;
+import xadrez.XadrezException;
 
 public class Program {
     public static void main(String[] args) throws Exception {
@@ -13,16 +15,25 @@ public class Program {
         PartidaXadrez partidaXadrez = new PartidaXadrez();
 
         while (true) {
-            UI.printBoard(partidaXadrez.getPecas());
-            System.out.println();
-            System.out.print("Origem: ");
-            PosicaoXadrez origem = UI.lerPosicaoXadrez(sc);
+            try {
+                UI.limparTela();
+                UI.printBoard(partidaXadrez.getPecas());
+                System.out.println();
+                System.out.print("Origem: ");
+                PosicaoXadrez origem = UI.lerPosicaoXadrez(sc);
 
-            System.out.println();
-            System.out.print("Destino: ");
-            PosicaoXadrez destino = UI.lerPosicaoXadrez(sc);
+                System.out.println();
+                System.out.print("Destino: ");
+                PosicaoXadrez destino = UI.lerPosicaoXadrez(sc);
 
-            PecaXadrez pecaCapturada = partidaXadrez.MovimentoXadrez(origem, destino);
+                PecaXadrez pecaCapturada = partidaXadrez.MovimentoXadrez(origem, destino);
+            } catch (XadrezException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
 
     }
